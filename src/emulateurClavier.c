4c0f329fd3701c566198bf3994071d33ebfb54bc
Karim Khiari <kakhi1@ulaval.ca>
2026-03-08 17:00:53 +0000

    add space characters before non space characters

--- src/emulateurClavier.c
+++ src/emulateurClavier.c
@@ -20,61 +20,63 @@ FILE* initClavier(){
 int ecrireCaracteres(FILE* periphClavier, const char* caracteres, size_t len, unsigned int tempsTraitementParPaquetMicroSecondes){
     // TODO ecrivez votre code ici. Voyez les explications dans l'enonce et dans
     // emulateurClavier.h
-    // numbers 48 - 57
-    // lower 97 - 122
-    // upper 65 - 90
     char buf[LONGUEUR_USB_PAQUET] = {0};
     size_t buf_pos;
     size_t i = 0;
     while (i < len){
         buf_pos = 2;
-        if (caracteres[i] >= 'A' && caracteres[i] <= 'Z'){ // upper
-            buf[0] = 2;
-            buf[buf_pos++] = caracteres[i++] - 'A' + 4;
-            while (buf_pos < LONGUEUR_USB_PAQUET && i < len){
-                if (caracteres[i] >= 'A' && caracteres[i] <= 'Z'){
-                    buf[buf_pos++] = caracteres[i++] - 'A' + 4;
-                } else if (caracteres[i] == ' '){
-                    buf[buf_pos++] = 44;
-                    ++i;
-                } else {
-                    if (caracteres[i] == '0'
-                        || caracteres[i] == ','
-                        || caracteres[i] == '.'
-                        || caracteres[i] == '\n'
-                        || (caracteres[i] >= 'a' && caracteres[i] <= 'z')
-                        || (caracteres[i] >= '1' && caracteres[i] <= '9')){
-                        break;
-                    }
-                    return -1;
-                }
-            }
-        } else {
-            while (buf_pos < LONGUEUR_USB_PAQUET && i < len){
-                if (caracteres[i] >= 'a' && caracteres[i] <= 'z'){ // lower
-                    buf[buf_pos++] = caracteres[i++] - 'a' + 4;
-                } else if (caracteres[i] >= '1' && caracteres[i] <= '9'){ // 1..9
-                    buf[buf_pos++] = caracteres[i++] - '1' + 30;
-                } else if (caracteres[i] == '0'){
-                    buf[buf_pos++] = 39;
-                    ++i;
-                } else if (caracteres[i] == ','){
-                    buf[buf_pos++] = 54;
-                    ++i;
-                } else if (caracteres[i] == '.'){
-                    buf[buf_pos++] = 55;
-                    ++i;
-                } else if (caracteres[i] == '\n'){
-                    buf[buf_pos++] = 40;
-                    ++i;
-                } else if (caracteres[i] == ' '){
-                    buf[buf_pos++] = 44;
-                    ++i;
-                } else {
+        while (caracteres[i] == ' ' && buf_pos < LONGUEUR_USB_PAQUET && i < len){ // space character can be used either with shift or not, we dont know which one until we encounter a non space character
+            buf[buf_pos] = 44;
+            ++i;
+            ++buf_pos;
+        }
+        if (buf_pos < LONGUEUR_USB_PAQUET && i < len){
+            if (caracteres[i] >= 'A' && caracteres[i] <= 'Z'){
+                buf[0] = 2;
+                buf[buf_pos++] = caracteres[i++] - 'A' + 4;
+                while (buf_pos < LONGUEUR_USB_PAQUET && i < len){
                     if (caracteres[i] >= 'A' && caracteres[i] <= 'Z'){
-                        break;
+                        buf[buf_pos] = caracteres[i] - 'A' + 4;
+                    } else if (caracteres[i] == ' '){
+                        buf[buf_pos] = 44;
+                    } else {
+                        if (caracteres[i] == '0'
+                            || caracteres[i] == ','
+                            || caracteres[i] == '.'
+                            || caracteres[i] == '\n'
+                            || (caracteres[i] >= 'a' && caracteres[i] <= 'z')
+                            || (caracteres[i] >= '1' && caracteres[i] <= '9')){
+                            break;
+                        }
+                        return -1;
                     }
-                    return -1;
+                    ++i;
+                    ++buf_pos;
+                }
+            } else {
+                while (buf_pos < LONGUEUR_USB_PAQUET && i < len){
+                    if (caracteres[i] >= 'a' && caracteres[i] <= 'z'){
+                        buf[buf_pos] = caracteres[i] - 'a' + 4;
+                    } else if (caracteres[i] >= '1' && caracteres[i] <= '9'){
+                        buf[buf_pos] = caracteres[i] - '1' + 30;
+                    } else if (caracteres[i] == '0'){
+                        buf[buf_pos] = 39;
+                    } else if (caracteres[i] == ','){
+                        buf[buf_pos] = 54;
+                    } else if (caracteres[i] == '.'){
+                        buf[buf_pos] = 55;
+                    } else if (caracteres[i] == '\n'){
+                        buf[buf_pos] = 40;
+                    } else if (caracteres[i] == ' '){
+                        buf[buf_pos] = 44;
+                    } else {
+                        if (caracteres[i] >= 'A' && caracteres[i] <= 'Z'){
+                            break;
+                        }
+                        return -1;
+                    }
+                    ++i;
+                    ++buf_pos;
                 }
             }
         }
